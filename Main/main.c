5fea1a670cf78f46c142303c0638d3c51794337c
Michael Krzyzaniak <michael.krzyzaniak@yahoo.com>
2019-11-22 12:48:30 +0100

    More debugging USB enumeration order

--- Main/main.c
+++ Main/main.c
@@ -31,7 +31,7 @@ typedef struct parameter_struct
 /*--------------------------------------------------------------------*/
 int main(void)
 {
-  fprintf(stderr, "Version 1.1\r\n");
+  fprintf(stderr, "Version 1.2\r\n");
   fprintf(stderr, "'q' to quit\r\n'<' or '>' to scroll through paramaters\r\n'+' or '-' to change the parameter values\r\n");
   
   int param_index = 0;
