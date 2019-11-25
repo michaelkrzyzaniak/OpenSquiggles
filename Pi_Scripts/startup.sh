2ef9a57cb98c709190a6d55d8006f17c0be1f070
Michael Krzyzaniak <michael.krzyzaniak@yahoo.com>
2019-11-25 05:47:01 +0100

    Don't launch on boot

--- Pi_Scripts/startup.sh
+++ Pi_Scripts/startup.sh
@@ -7,4 +7,4 @@ git pull
 cd /home/pi/Dr_Squiggles/Main
 sudo apt-get install libasound2-dev
 gcc *.c ../Robot_Communication_Framework/*.c ../Beat-and-Tempo-Tracking/src/*.c Rhythm_Generators/*.c -lasound -lm -lpthread -O2
-/home/pi/Dr_Squiggles/Main/a.out
+#/home/pi/Dr_Squiggles/Main/a.out
