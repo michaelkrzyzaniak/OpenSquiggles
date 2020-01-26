5545828a7c2614d02ab63e7b68da16ebfc820ea7
Michael Krzyzaniak <mkrzyzan@asu.edu>
2020-01-26 11:04:51 +0100

    Linux tests

--- Main/Rhythm_Generators/Rhythm_OSC.c
+++ Main/Rhythm_Generators/Rhythm_OSC.c
@@ -196,10 +196,12 @@ void* rhythm_OSC_recv_thread_run_loop(void* SELF)
   for(;;)
   {
     int num_valid_bytes = net_udp_receive (self->net, self->osc_recv_buffer, OSC_BUFFER_SIZE, senders_address);
+    fprintf(stderr, "HERE  1\r\n");
     if(num_valid_bytes < 0)
       continue; //return NULL ?
   
     int num_osc_values = oscParse(self->osc_recv_buffer, num_valid_bytes, &osc_address, &osc_type_tag, self->osc_values_buffer, OSC_VALUES_BUFFER_SIZE);
+    fprintf(stderr, "HERE  2\r\n");
     if(num_osc_values < 0)
         continue;
   
@@ -207,14 +209,18 @@ void* rhythm_OSC_recv_thread_run_loop(void* SELF)
     if(address_hash == 3170568888) // '/rhythm'
       {
         // /rhythm <int>robot_id <int>beat_id <float 0~1>time_1 <int 0~127>midi_note_number_1 <float 0~1>strength_1 <float 0~1>time_2 <int 0~127>midi_note_number_2 <float 0~1>strength_2 ...
-      
+      fprintf(stderr, "HERE  3\r\n");
         if(num_osc_values < 2)     continue; //must have robot_id and beat_id
+        fprintf(stderr, "HERE  4\r\n");
         if((num_osc_values-2) % 3) continue; //after that, must have onset triplets
+        fprintf(stderr, "HERE  5\r\n");
         int robot_id = oscValueAsInt(self->osc_values_buffer[0], osc_type_tag[0]);
         int beat_id  = oscValueAsInt(self->osc_values_buffer[1], osc_type_tag[1]);
       
         if((robot_id != self->robot_id) || (beat_id != self->beat_id))
           continue;
+      
+      fprintf(stderr, "HERE  6\r\n");
       
         int i, j=0;
         pthread_mutex_lock(&self->onset_buffer_mutex);
@@ -227,6 +233,8 @@ void* rhythm_OSC_recv_thread_run_loop(void* SELF)
           }
         self->num_onsets = j;
         pthread_mutex_unlock(&self->onset_buffer_mutex);
+      
+        fprintf(stderr, "num_onsets  %i\r\n\r\n\r\n", j);
       }
   }
   
