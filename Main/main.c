e6d7d4255ffc135f0b470c95aa125a6b70bed035
Michael Krzyzaniak <michael.krzyzaniak@yahoo.com>
2020-05-07 15:28:10 +0200

    Update Version Number

--- Main/main.c
+++ Main/main.c
@@ -16,7 +16,7 @@
 //sudo apt-get install libasound2-dev
 //gcc *.c ../Robot_Communication_Framework/*.c ../Beat-and-Tempo-Tracking/src/*.c Rhythm_Generators/*.c extras/*.c -lasound -lm -lpthread -lrt -O2
 
-#define __SQ_VERSION__ "1.22"
+#define __SQ_VERSION__ "1.23"
 
 #include "Microphone.h"
 #include <string.h> //strcmp
