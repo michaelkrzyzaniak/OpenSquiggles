eb743238c15c7b6dc3cedeb3063e725885ae1576
Michael Krzyzaniak <michael.krzyzaniak@yahoo.com>
2021-02-13 21:11:08 +0100

    Fix compile script

--- Pi_Scripts/startup.sh
+++ Pi_Scripts/startup.sh
@@ -10,6 +10,6 @@ cd /home/pi/Dr_Squiggles/Main
 sudo apt-get install libasound2-dev
 gcc sq.c core/*.c ../Robot_Communication_Framework/*.c ../Beat-and-Tempo-Tracking/src/*.c Rhythm_Generators/*.c extras/*.c -lasound -lm -lpthread -lrt -O2 -o sq
 sudo cp ./sq /usr/local/bin/sq
-gcc op.c core/*.c ../Robot_Communication_Framework/*.c ../Beat-and-Tempo-Tracking/src/*.c extras/*.c -lasound -lm -lpthread -lrt -O2 -o op
+gcc op.c core/*.c ../Robot_Communication_Framework/*.c ../Beat-and-Tempo-Tracking/src/*.c Rhythm_Generators/*.c extras/*.c -lasound -lm -lpthread -lrt -O2 -o op
 sudo cp ./op /usr/local/bin/op
 #/home/pi/Dr_Squiggles/Main/a.out
